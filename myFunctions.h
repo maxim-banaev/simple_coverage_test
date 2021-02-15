a6b2876508c455dd8836b02c570082f15e27e50c
Maxim Banaev <Maxim.Banaev@jetbrains.com>
2021-02-15 20:20:31 +0300

    update

--- myFunctions.h
+++ myFunctions.h
@@ -7,11 +7,47 @@ int functionWithoutBranch(int a) {
     return a;
 }
 
-int functionWithSingleIfBranch(int a) {
+int functionWithIfBranch(int a) {
     std::cout << "Get input value: " << a << std::endl;
     // increase input value if positive
     if (a > 0) {
         a++;
+    }
+    return a;
+}
+
+int functionWithIfElseBranch(int a) {
+    std::cout << "Get input value: " << a << std::endl;
+    // increase input value if positive
+    if (a > 0) {
+        a++;
+    } else {
+        a--;
+    }
+    return a;
+}
+
+int functionWithDoubleIfBranch(int a) {
+    std::cout << "Get input value: " << a << std::endl;
+    // increase input value if positive
+    if (a > 0) {
+        a++;
+    }
+    if (a < 0) {
+        a--;
+    }
+    return a;
+}
+
+int functionWithCaseBranch(int a) {
+    std::cout << "Get input value: " << a << std::endl;
+    switch (a) {
+        case 1:
+            a *=10;
+            break;
+        default:
+            a = 0;
+            break;
     }
     return a;
 }
--- test.cpp
+++ test.cpp
@@ -1,14 +1,24 @@
 #define CATCH_CONFIG_MAIN
-#include "catch.hpp"
+#include "catch/catch.hpp"
 
 #include "myFunctions.h"
 
 TEST_CASE("check functionWithoutBranch", "[functionWithoutBranch]") {
     CHECK(functionWithoutBranch(0) == 1);
-    CHECK(functionWithoutBranch(1) == 2);
 }
 
-TEST_CASE("check functionWithSingleIfBranch", "[functionWithSingleIfBranch]") {
-    CHECK(functionWithSingleIfBranch(0) == 0);
-    //CHECK(functionWithSingleIfBranch(1) == 2);
+TEST_CASE("check functionWithIfBranch", "[functionWithIfBranch]") {
+    CHECK(functionWithIfBranch(1) == 2);
+}
+
+TEST_CASE("check functionWithIfElseBranch", "[functionWithIfElseBranch]") {
+    CHECK(functionWithIfElseBranch(1) == 2);
+}
+
+TEST_CASE("check functionWithDoubleIfBranch", "[functionWithDoubleIfBranch]") {
+    CHECK(functionWithDoubleIfBranch(1) == 2);
+}
+
+TEST_CASE("check functionWithCaseBranch", "[functionWithCaseBranch]") {
+    CHECK(functionWithCaseBranch(1) == 10);
 }
